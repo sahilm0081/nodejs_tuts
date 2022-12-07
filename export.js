244107e005ba30652fb2759e2762802561677a35
Sahil Madan <sahil.madan@mymedisage.com>
2022-12-07 06:39:51 +0530

    add import and export file

---
+++ export.js
@@ -0,0 +1,9 @@
+const data=["hello","hello1","hello2"];
+const age=[23,12,21];
+
+module.exports = {
+    data,
+    age
+}
+
+
---
+++ import.js
@@ -0,0 +1,5 @@
+const {data,age} =require("./export");
+console.log(data);
+console.log(age);
+
+
