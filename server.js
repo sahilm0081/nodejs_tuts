614ffe0c35768a1c8db941f61a27631b0c65d0df
Sahil Madan <sahil.madan@mymedisage.com>
2022-12-08 20:51:36 +0530

    install express

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const http=require("http")
-const server=http.createServer((req,res)=>{
-console.log("request coming");
-        res.setHeader("Content-type","text/html")
-        res.write("hello")
-        res.end();
+const express=require("express")
+
+const app =express()
+
+app.get("/",(req,res)=>{
+    res.send("here from express");
+
 });
-server.listen(3000,"localhost",()=>{
-    console.log("listening for req on port 3000")
-})
+
+app.listen(3000)
+
+
+// const http=require("http")
+// const server=http.createServer((req,res)=>{
+// console.log("request coming");
+//         res.setHeader("Content-type","text/html")
+//         res.write("hello")
+//         res.end();
+// });
+// server.listen(3000,"localhost",()=>{
+//     console.log("listening for req on port 3000")
+// })
